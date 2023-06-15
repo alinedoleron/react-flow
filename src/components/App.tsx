03a4a58c90b4cad176188289feb988cb16e30dd2
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-06-15 16:18:29 -0300

    Add node resizer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,55 +1,84 @@
-import React, { useCallback } from 'react';
-import ReactFlow, { addEdge, Connection,useNodesState, useEdgesState,Background, Controls, ConnectionMode}from 'reactflow';
+import React, { useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  Connection,
+  useNodesState,
+  useEdgesState,
+  Background,
+  Controls,
+  ConnectionMode,
+} from "reactflow";
+import * as Toolbar from "@radix-ui/react-toolbar";
+import { Square } from "../components/nodes/Square";
 
-import {Square} from '../components/nodes/Square';
-
-import 'reactflow/dist/style.css';
-import DefaultEdge from './edges/DefaultEdge';
+import "reactflow/dist/style.css";
+import DefaultEdge from "./edges/DefaultEdge";
+import "./App.css";
 
 const NODE_TYPES = {
-  square: Square
-}
+  square: Square,
+};
 
 const EDGE_TYPES = {
-  default: DefaultEdge
-}
+  default: DefaultEdge,
+};
 
 const initialNodes = [
-  { id: '1', position: { x: 100, y: 300 }, type: 'square', data: { label: 'Node 1' } },
-  { id: '2', position: { x: 300, y: 300 }, type: 'square', data: { label: 'Node 2' } },
-] ;
+  {
+    id: "1",
+    position: { x: 100, y: 300 },
+    type: "square",
+    data: { label: "Node 1" },
+  },
+  {
+    id: "2",
+    position: { x: 300, y: 300 },
+    type: "square",
+    data: { label: "Node 2" },
+  },
+];
 // const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 export default function App() {
-
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
 
   const onConnect = useCallback((connection: Connection) => {
-    return setEdges(edges => addEdge(connection, edges))
+    return setEdges((edges) => addEdge(connection, edges));
   }, []);
 
+  function addSquareNode() {
+    setNodes((nodes) => [
+      ...nodes,
+      {
+        id: "3",
+        position: { x: 700, y: 350 },
+        type: "square",
+        data: { label: "Node 1" },
+      },
+    ]);
+  }
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow 
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
         connectionMode={ConnectionMode.Loose}
-        defaultEdgeOptions={{type: 'default'}}
+        defaultEdgeOptions={{ type: "default" }}
         edges={edges}
         edgeTypes={EDGE_TYPES}
         nodes={nodes}
-        nodeTypes={NODE_TYPES} 
+        nodeTypes={NODE_TYPES}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodesChange={onNodesChange}
       >
-        <Background
-          gap={12}
-          size={2}
-          color="#eee" 
-          />
-        <Controls/>
+        <Background gap={12} size={2} color="#eee" />
+        <Controls />
       </ReactFlow>
 
+      <Toolbar.Root className="toolbar">
+        <Toolbar.Button className="button" onClick={addSquareNode} />
+      </Toolbar.Root>
     </div>
   );
 }
--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -1,37 +1,52 @@
 import React from "react";
-import {NodeProps, Handle, Position} from "reactflow";
-import './Square.css';
+import { NodeResizer, NodeProps, Handle, Position } from "reactflow";
+import "./Square.css";
 
-export function Square(params:NodeProps) {
-    return (
-        <div style={{backgroundColor: '#5ba6cf', borderRadius: '5px',  width: '100px', height: '100px'}} >
-            {params.data.label}
-            <Handle
-                id="right" 
-                position={Position.Right}
-                type={"source"}
-                className={'handle handle-right'}
-                />
-            <Handle
-                id="left"
-                position={Position.Left}
-                type={"source"}
-                className={'handle handle-left'}
-                />
-            <Handle
-                id="top" 
-                position={Position.Top}
-                type={"source"}
-                className={'handle handle-top'}
-                />
-            <Handle
-                id="bottom"
-                position={Position.Bottom}
-                type={"source"}
-                className={'handle handle-bottom'}
-                />
-        </div>    
-    )
-        
-    
+export function Square({selected}: NodeProps) {
+  return (
+    <div
+      style={{
+        backgroundColor: "#5ba6cf",
+        borderRadius: "5px",
+        height: "100%",
+        minHeight: "100px",
+        minWidth: "100px",
+        width: "100%",
+
+      }}
+    >
+      <NodeResizer
+        isVisible={selected}
+        minHeight={100}
+        minWidth={100}
+        lineClassName={"line-resizer"}
+        handleClassName={"handle-resizer"}
+         />
+
+      <Handle
+        id="right"
+        position={Position.Right}
+        type={"source"}
+        className={"handle handle-right"}
+      />
+      <Handle
+        id="left"
+        position={Position.Left}
+        type={"source"}
+        className={"handle handle-left"}
+      />
+      <Handle
+        id="top"
+        position={Position.Top}
+        type={"source"}
+        className={"handle handle-top"}
+      />
+      <Handle
+        id="bottom"
+        position={Position.Bottom}
+        type={"source"}
+        className={"handle handle-bottom"}
+      />
+    </div>
+  );
 }
